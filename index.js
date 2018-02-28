aeae510a2079119e51d78a88e5d6af658461388e
Mathieu Bouchard <matb33@gmail.com>
2018-02-28 15:39:57 -0500

    try to simplify index.js so there's no es6 const etc

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const createConnect = require('./lib/createConnect')
-const createStore = require('./lib/createStore')
-const MemoryStorage = require('./lib/MemoryStorage')
-
-const localStorageStore = require('./lib/localStorageStore')
-const memoryStorageStore = require('./lib/memoryStorageStore')
-const sessionStorageStore = require('./lib/sessionStorageStore')
-
-const withLocalStorage = require('./lib/withLocalStorage')
-const withMemoryStorage = require('./lib/withMemoryStorage')
-const withSessionStorage = require('./lib/withSessionStorage')
-
 module.exports = {
-  createConnect,
-  createStore,
-  MemoryStorage,
-  localStorageStore,
-  memoryStorageStore,
-  sessionStorageStore,
-  withLocalStorage,
-  withMemoryStorage,
-  withSessionStorage
+  createConnect: require('./lib/createConnect').default,
+  createStore: require('./lib/createStore').default,
+  MemoryStorage: require('./lib/MemoryStorage').default,
+  localStorageStore: require('./lib/localStorageStore').default,
+  memoryStorageStore: require('./lib/memoryStorageStore').default,
+  sessionStorageStore: require('./lib/sessionStorageStore').default,
+  withLocalStorage: require('./lib/withLocalStorage').default,
+  withMemoryStorage: require('./lib/withMemoryStorage').default,
+  withSessionStorage: require('./lib/withSessionStorage').default
 }
