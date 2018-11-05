adc7f0b443be3a6a32f413b10ca04d5eb110afc4
Mathieu Bouchard <matb33@gmail.com>
2018-11-05 16:03:03 -0500

    rebuild lib

--- lib/LocalStorage.d.ts
+++ lib/LocalStorage.d.ts
@@ -1,4 +1,5 @@
 export default class LocalStorage {
+    constructor();
     setStore(store: any): void;
     getStore(): any;
 }
--- lib/SessionStorage.d.ts
+++ lib/SessionStorage.d.ts
@@ -1,4 +1,5 @@
 export default class SessionStorage {
+    constructor();
     setStore(store: any): void;
     getStore(): any;
 }
